09d7db35212e6af5894d481b6855832021e5c1d8
Bruno Gomes <brunompg@gmail.com>
2015-08-31 11:50:40 +0100

    Added Sass support

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 /* File: gulpfile.js */
 
 // grab our packages
-var gulp   = require('gulp'),
-    jshint = require('gulp-jshint');
+var gulp   		= require('gulp'),
+    jshint 		= require('gulp-jshint'),
+    sass 		= require('gulp-sass'),
+    sourcemaps 	= require('gulp-sourcemaps');
 
 // define the default task and add the watch task to it
 gulp.task('default', ['watch']);
@@ -14,7 +16,17 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
+// configure Sass
+gulp.task('build-css', function(){
+	return gulp.src('source/scss/**/*.scss')
+		.pipe(sourcemaps.init())	// Process the original sources
+		.pipe(sass())
+		.pipe(sourcemaps.write())	// Add the map to modified source
+		.pipe(gulp.dest('public/assets/stylesheets'));
+});
+
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
   gulp.watch('source/javascript/**/*.js', ['jshint']);
+  gulp.watch('source/scss/**/*.scss', ['build-css']);
 });
