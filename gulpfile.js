f55f3b95f964e9cc0480c577766bf18eadafb79b
Bruno Gomes <brunompg@gmail.com>
2015-08-31 12:48:04 +0100

    Added js concat and minify task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@
 
 // grab our packages
 var gulp   		= require('gulp'),
+	gutil		= require('gulp-util'),
     jshint 		= require('gulp-jshint'),
     sass 		= require('gulp-sass'),
-    sourcemaps 	= require('gulp-sourcemaps');
+    sourcemaps 	= require('gulp-sourcemaps'),
+    concat		= require('gulp-concat'),
+    uglify		= require('gulp-uglify');
 
 // define the default task and add the watch task to it
 gulp.task('default', ['watch']);
@@ -25,8 +28,19 @@ gulp.task('build-css', function(){
 		.pipe(gulp.dest('public/assets/stylesheets'));
 });
 
+// configure js concat and minify
+gulp.task('build-js', function() {
+	return gulp.src('source/javascript/**/*.js')
+		.pipe(sourcemaps.init())
+		.pipe(concat('bundle.js'))
+		// only uglify if gulp is ran with '--type production'
+		.pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('public/assets/javascript'));
+});
+
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
-  gulp.watch('source/javascript/**/*.js', ['jshint']);
+  gulp.watch('source/javascript/**/*.js', ['jshint', 'build-js']);
   gulp.watch('source/scss/**/*.scss', ['build-css']);
 });
